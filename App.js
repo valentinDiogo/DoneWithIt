eac262fea9c4e63cd4967b8f9439a0becf62b7d9
valentinDiogo <64988233+valentinDiogo@users.noreply.github.com>
2022-04-13 17:48:33 +0200

    Add: ImageInput and FormImagePicker components

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 import LoginScreen from "./app/screens/LoginScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return <ListingEditScreen />;
